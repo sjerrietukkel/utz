7a39e758c478eca9b53be8a54bf3d2ca666123c8
sjerrietukkel <danielvdschuur@gmail.com>
2019-01-18 15:02:28 +0100

    messing around pre weekend

--- js/d3.js
+++ js/d3.js
@@ -1,44 +1,31 @@
-// Select your div
-const waffle = d3.select('.waffle')
-
-// Create an array with numbers 0 - 99
-const numbers = d3.range(100)
-
-// For each item in the array, add a div element
-// if the number is < 5, color it red, otherwise gray
-waffle
-	.selectAll('.block')
-	.data(numbers)
-	.enter()
-	.append('div')
-	.attr('class', 'block')
-	.style('background-color', d => (d < 32 ? '#FE4A49' : '#CCCCCC'))
-	
-
-
 d3.json("/databron/cleaned_dataset_kit.json").then(function(data) {
 	var byppi = d3.nest()
 		.key(function(d) { return d.ppi })
 		.entries(data)
 
-	// const svg = d3.select(DOM.svg(width, height))
-    //   .style("width", "100%")
-    //   .style("height", "auto")
-	//   .style("font", "10px sans-serif")
-	  
-	// const leaf = svg.selectAll("g")
-	// 	.data(root.leaves())
-	// 	.enter().append("g")
-	// 	.attr("transform", d => `translate(${d.x0},${d.y0})`)
+	// var mappedValues = byppi.values.map(d => {
+    //     return {
+    //         avocado: byppi.values[0].Avocado
+    //     }
+	// })
+	
+	// console.log(mappedValues)
 
-console.log(byppi)
-// console.log(byppi[0].values[0].Cassava)
-// console.log(byppi[0].values[0].Cacao)
+	const waffle = d3.select('.waffle')
+	const numbers = d3.range(100)
 
-
+	waffle
+		.selectAll('.block')
+		.data(numbers)
+		.enter()
+		.append('div')
+		.attr('class', 'block')
+		.style('background-color', d => (d < 32 ? '#FE4A49' : '#CCCCCC'))
+	
+	console.log(byppi)
+	console.log(byppi[0].values[0].Hectare)
+	console.log(byppi[0].values[0].Cacao)
 })
-
-
-  .catch(function(d){
-	  console.log("Hij laadt niet, vriend.")
-  })
+.catch(function(){
+	console.log("kut.")
+})
